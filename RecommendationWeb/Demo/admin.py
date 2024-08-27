17b877536f625557b497e3dafd8747d8facc78a7
thinh <hoangthinh110304@gmail.com>
2024-08-27 22:22:00 +0700

    model

--- RecommendationWeb/Demo/admin.py
+++ RecommendationWeb/Demo/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import FieldOfStudy, University, Major, UniMajor
+from .models import FieldOfStudy, University, Major, UniMajor, Campus, Combination, CombMajor, Region, UniField
 
 admin.site.register(FieldOfStudy)
 admin.site.register(University)
 admin.site.register(Major)
 admin.site.register(UniMajor)
+admin.site.register(Campus)
+admin.site.register(Combination)
+admin.site.register(CombMajor)
+admin.site.register(Region)
+admin.site.register(UniField)
