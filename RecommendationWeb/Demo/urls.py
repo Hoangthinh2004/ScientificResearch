f7ae753a5514ad8b45f0cb377eb4da5db432d540
NhanNPSE184696 <nhannpse184696@fpt.edu.vn>
2024-08-29 22:59:27 +0700

    region + type filter

--- RecommendationWeb/Demo/urls.py
+++ RecommendationWeb/Demo/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     path('university/<str:pk>', views.showOneUniversity, name="show-one-university"),
     path('major/', views.listMajor, name="list-major"),
     path('major/<str:pk>', views.showOneMajor, name="show-one-major"),
-    path('unimajor/<str:uid>/<str:mid>', views.showOneMajor, name="show-one-major"),
+    path('university/<str:pk>', views.showOneUniversity, name="show-one-university"),
+    path('filter/', views.filter, name="filter"),
+    path('university/filter/', views.filter, name="filter-university"),
+    path('major/filter/', views.filter, name="filter-major"),
     
 
 ]
--- RecommendationWeb/Demo/views.py
+++ RecommendationWeb/Demo/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404, get_list_or_40
 from django.http import HttpResponse, HttpRequest
 from .models import University, Major, FieldOfStudy, UniMajor, Region, Campus, CombMajor
 from django.db.models import Q
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
+    url = 'filter'
     unis = University.objects.all()
     majors = Major.objects.all()
     fieldOfMajors = FieldOfStudy.objects.all()
@@ -16,110 +18,95 @@ def home(request):
                                     Q(Description__icontains=s) 
                                     )
         majors = Major.objects.filter(Q(ID__icontains=s) |
-                                    Q(Name__icontains=s) |
-                                    Q(FieldID__ID__icontains=s) |
-                                    Q(FieldID__Name__icontains=s) 
-                                    )
-        
-    context = {'unis': unis, 'majors': majors, 'fieldOfMajors': fieldOfMajors}
+                                    Q(Name__icontains=s))
+    context = {'unis': unis, 'majors': majors, 'fieldOfMajors': fieldOfMajors, 'url': reverse(url)}
     return render(request, 'Demo/home.html', context)
 
 def listUniversity(request):
+    url = 'filter-university'
     unis = University.objects.all()
-    if request.method == 'GET':
+    if request.method == 'GET' and request.GET.get('u') is not None:
         u = request.GET.get('u')
         unis = University.objects.filter(Q(ID__icontains=u) |
                                     Q(Name__icontains=u))
-    context = {'unis': unis}
+    context = {'unis': unis, 'url': reverse(url)}
     return render(request,'Demo/uni.html', context)
 
 def showOneUniversity(request,pk):
+    url = 'filter-university'
     uni = University.objects.get(ID=pk)
-    uni_majors = UniMajor.objects.filter(UniID__ID=pk)
-    comb_majors = CombMajor.objects.filter(Uni__ID=pk)
-    context = {'uni': uni, 'uni_majors': uni_majors, 'comb_majors': comb_majors}
+    uni_majors = UniMajor.objects.filter(Q(UniID__ID=pk))
+    comb_majors = CombMajor.objects.filter(Q(Uni__ID=pk))
+    context = {'uni': uni, 'uni_majors': uni_majors, 'comb_majors': comb_majors, 'url': reverse(url)}
     return render(request,'Demo/uni.html', context)
 
 def unimajor(request, uid, mid):
     uni_major = get_object_or_404(UniMajor, Q(UniID=uid) | Q(MajorID=mid))
-    context = {'unimajor': unimajor}
+    context = {'unimajor': unimajor, 'url': reverse(url)}
     return render(request,'', context)
 
 def listMajor(request):
+    url = 'filter-major'
     majors = UniMajor.objects.all()
     if request.method == 'GET' and request.GET.get('m') is not None:
         m = request.GET.get('m')
-        majors = UniMajor.objects.filter(Q(MajorID__ID__icontains=m) |
-                                    Q(MajorID__Name__icontains=m))
-    context = {'majors': majors}
+        majors = UniMajor.objects.filter(Q(MajorID__ID__icontains=m) | Q(MajorID__Name__icontains=m))
+    context = {'majors': majors, 'url': reverse(url)}
     return render(request,'Demo/major.html', context)
 
 def showOneMajor(request, pk):
+    url = 'filter-major'
     major = Major.objects.get(ID=pk)
     uni_majors = UniMajor.objects.filter(Q(MajorID__FieldID__ID=pk) | Q(MajorID__ID=pk))
-    comb_majors = CombMajor.objects.filter(Major__ID=pk, Major__FieldID__ID=pk)
-    context = {'uni_majors': uni_majors, 'comb_majors': comb_majors, 'major': major}
+    comb_majors = CombMajor.objects.filter(Q(Major__FieldID__ID=pk) | Q(Major__ID=pk))
+    context = {'uni_majors': uni_majors, 'comb_majors': comb_majors, 'major': major, 'url': reverse(url)}
     return render(request,'Demo/major.html' , context)
 
-def filterRegion(request):
-    major_id = request.GET.get('major_id', '')
-    uni_id = request.GET.get('uni_id', '')
-    region_id = request.GET.get('region', '')
-    if (major_id != '' and uni_id != ''):
-        uni_majors = getUniMajorByRegionAndMajorAndUniversity(region_id, major_id, uni_id)
-        return render(request, 'Demo/major.html', {'uni_majors': uni_majors})
-    elif major_id != '':
-        majors = getUniMajorByRegionAndMajor(region_id, major_id)
-        return render(request, 'Demo/major.html', {'majors': majors})
-    else:
-        unis = getUniMajorByRegionAndUniversity(region_id, uni_id)
-        return render(request, 'Demo/uni.html', {'unis': unis})
-
-def getUniMajorByRegionAndMajorAndUniversity(region_id, major_id, uni_id):
-    checkExist = Campus.objects.filter(Region__ID=region_id, Uni__ID=uni_id).exists()
-    if checkExist:
-        uni_majors = UniMajor.objects.filter(UniID__ID=uni_id, MajorID__ID=major_id)
-        return uni_majors
-    else:
-        return UniMajor.objects.none()
+def filter(request):
+    unis = University.objects.all()
+    majors = Major.objects.all()
+    uni_majors = UniMajor.objects.all()
+    #Filtering by region
+    selected_regions = request.GET.getlist('region')
+    if selected_regions:
+        campuses = Campus.objects.filter(Region__ID__in=selected_regions).values_list('Uni', flat=True)
+        unis = unis.filter(ID__in=campuses)
+    #Filtering by type
+    selected_types = request.GET.getlist('type')
+    if selected_types:
+        unis = unis.filter(Type__in=selected_types)
+    #Filtering by fee
+    selected_fees = request.GET.getlist('fee')
+    if selected_fees:
+        fees = [tuple(map(float, fee.split('-'))) for fee in selected_fees]
+        queries = [Q(Min_Tuition_Fee__gt=fee[0], Max_Tuition_Fee__lte=fee[1]) for fee in fees]
+        query = queries.pop()
+        for q in queries:
+            query |= q
+        unis = unis.filter(query)
+    #Filtering by score
+    selected_scores = request.GET.getlist('score')
+    if selected_scores:
+        scores = [tuple(map(float, score.split('-')) for score in selected_scores)]
+        queries = [Q(Min_Score__gt=score[0], Max_Score__lte=score[1]) for score in scores]    
+        query = queries.pop()
+        for q in query:
+            query |= q
+        unis = unis.filter(query)
+    uni_majors = uni_majors.filter(UniID__ID__in=unis)
+    majors = majors.filter(ID__in=uni_majors)
+    query_params = request.GET.copy().urlencode()
+    context = {'unis': unis, 'uni_majors': uni_majors, 'majors': majors}
+    # Reconstruct query parameters for the URL
     
-def getUniMajorByRegionAndMajor(region_id, major_id):
-    campus = Campus.objects.filter(Region__ID=region_id)
-    university_ids = campus.values_list('Uni__ID', flat=True)
-    majors = UniMajor.objects.filter(UniID__ID__in=university_ids, MajorID__ID=major_id)
-    return majors
 
-def getUniMajorByRegionAndUniversity(region_id, uni_id):
-    checkExist = Campus.objects.filter(Region__ID=region_id, Uni__ID=uni_id).exists()
-    if checkExist:
-        uni_majors = UniMajor.objects.filter(UniID__ID=uni_id)
-        return uni_majors
+    # Determine the referring page and redirect back to it
+    if 'university' in request.path:
+         url = 'Demo/uni.html'
+    elif 'major' in request.path:
+        url = 'Demo/major.html'
     else:
-        return UniMajor.objects.none()
-    
-def filterType(request):
-    major_id = request.GET.get('major_id', '')
-    uni_id = request.GET.get('uni_id', '')
-    uni_type = request.GET.get('type', '')
-    if (major_id != '' and uni_id != ''):
-        uni_majors = getUniMajorByTypeAndMajorAndUniversity(uni_type, major_id, uni_id)
-        return render(request, 'Demo/major.html', {'uni_majors': uni_majors})
-    elif major_id != '':
-        majors = getUniMajorByTypeAndMajor(uni_type, major_id)
-        return render(request, 'Demo/major.html', {'majors': majors})
-    else:
-        unis = getUniMajorByTypeAndUniversity(uni_type, uni_id)
-        return render(request, 'Demo/uni.html', {'unis': unis})
-    
-def getUniMajorByTypeAndMajorAndUniversity(type, major_id, uni_id):
-    uni_majors = UniMajor.objects.filter(UniID__ID=uni_id, UniID__Type=type, MajorID=major_id)
-    return uni_majors
+        url = 'Demo/home.html'
 
-def getUniMajorByTypeAndMajor(type, major_id):
-    uni_majors = UniMajor.objects.filter(UniID__Type=type, MajorID=major_id)
-    return uni_majors
+    return render(request, url, context)
 
-def getUniMajorByTypeAndUniversity(type, uni_id):
-    uni_majors = UniMajor.objects.filter(UniID__ID=uni_id, UniID__Type=type)
-    return uni_majors
-    
